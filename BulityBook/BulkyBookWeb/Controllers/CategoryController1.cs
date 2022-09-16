d1abc9bae6edf40ddb55e8b3970a21f49cb73f7c
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-16 04:27:26 +0200

    Conecting with data base and table in Category page

---
+++ BulityBook/BulkyBookWeb/Controllers/CategoryController1.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController1(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
